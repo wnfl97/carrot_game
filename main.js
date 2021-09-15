37f46cced70e5d6416fa26eddd0ca86f7f62c361
wnfl97 <85559294+wnfl97@users.noreply.github.com>
2021-09-15 21:38:07 +0900

    html&css-self_markup

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 const startBtn = document.querySelector('.start__btn');
 const stopBtn = document.querySelector('.stop__btn');
 
-addEventListener('load',()=>{
-    const targetRect = startBtn.getBoundingClientRect();
-    startBtn.style.transform = `translate(${x}px, ${y}px)`
+setTimeout(() => {
+
 })
